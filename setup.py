202cf87ac3c85005e7f53bb17f079bcaacb9c359
Dariusz Kowalczyk <darton@users.noreply.github.com>
2025-03-05 03:00:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stts75m2f',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
     install_requires=['smbus2'],
     author='Dariusz Kowalczyk',
